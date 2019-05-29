117cf7532d7f3b4d4d76d576479706a57d006031
Miguel González <mgonzalez@udc.edu.co>
2019-05-29 15:31:41 -0500

    Primer commit

---
+++ src/main/java/com/imcoding/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,108 @@
+package com.imcoding.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@NotEmpty(message = "No puede estar vacío")
+	@Size(min=4, max=12)
+	@Column(nullable=false)
+	private String nombre;
+	
+	@NotEmpty(message = "No puede estar vacío")
+	private String apellido;
+	
+	@NotEmpty(message = "No puede estar vacío")
+	@Email
+	@Column(nullable=false, unique=true)
+	private String email;
+	
+	@NotNull(message = "No puede estar vacío")
+	@Column(name="created_at")
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+	
+	private String foto;
+	
+	/*@PrePersist
+	public void prePersist() {
+		createdAt = new Date();
+	}*/
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, created_at) VALUES ('Miguel', 'González', 'miguel.gonza3@gmail.com', '2019-05-26');
+INSERT INTO clientes (nombre, apellido, email, created_at) VALUES ('Yury', 'Vanegas', 'yury@gmail.com', '2018-05-26');
+INSERT INTO clientes (nombre, apellido, email, created_at) VALUES ('German', 'Federico', 'german@gmail.com', '2017-05-26');
+INSERT INTO clientes (nombre, apellido, email, created_at) VALUES ('Glenia', 'Gaviria', 'Glenia@gmail.com', '2019-05-26');
+INSERT INTO clientes (nombre, apellido, email, created_at) VALUES ('Luifer', 'Serpa', 'luifer@gmail.com', '2015-05-26');
